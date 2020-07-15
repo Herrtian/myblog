8df3363d5331b6e910436823e4f9a666dcfbf7f6
tiantian <1020116397@qq.com>
2020-07-15 14:24:47 +0800

    文章详情写好了的

--- article/urls.py
+++ article/urls.py
@@ -6,4 +6,7 @@ app_name = 'article'
 urlpatterns = [
     # path函数将url映射到视图
     path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+
+
 ]
--- article/views.py
+++ article/views.py
@@ -11,3 +11,11 @@ def article_list(request):
     context = { 'articles': articles }
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = { 'article': article }
+    # 载入模板，并返回context对象
+    return render(request, 'article/detail.html', context)
